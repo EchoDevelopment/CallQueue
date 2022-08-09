9bdf3e28e77adb80af18fb9bf923438b2241c14c
Echo <96091489+EchoDevelopment@users.noreply.github.com>
2022-08-08 17:28:05 -0700

    tuple method

--- DestroyQueue/src/main.cpp
+++ DestroyQueue/src/main.cpp
@@ -1,25 +1,55 @@
 #include <functional>
 #include <iostream>
 
-void sum(int* x, int* y)
+void sum(int x, int y)
 {
-	std::cout << *x + *y << std::endl;
+	std::cout << x + y << std::endl;
 }
+
+#define STD ::std::
 
 template <class func, class... types>
-std::function<void()> addQueue(func&& fn, size_t level, types&&... args)
+class FuncArg
 {
-	// Get ptr to args
-	// Create function that when the queue level is called it derefrences args and passes them to the actual function
-	// Return created function, bound properly
+public:
+	using tupleType = STD tuple<STD _Unrefwrap_t<types>...>;
+private:
+	func _function;
+	tupleType _args;
 
-	return std::_Binder<std::_Unforced, func, types...>(std::forward<func>(fn), std::forward<types>(args)...);
-}
+public:
+	FuncArg(func&& fn, size_t level, tupleType args)
+	{
+		_function = STD forward<func>(fn);
+		_args = STD forward<tupleType>(args);
+	}
+
+	void call()
+	{
+		STD apply(STD forward<func>(_function), STD forward<tupleType>(_args));
+	}
+};
+
+template <class func, class... types>
+FuncArg<func, types...>&& addQueue(func&& fn, size_t level, types&&... args)
+{
+
+	// New plan: store args
+	// Bind args and call function at the same time
+	// Keep binding if function is called multiple times
+	// Destroy vars when queue or that level of the queue should be destroyed
+
+	FuncArg<func, types...> funcArg(&sum, 4, STD make_tuple((args)...));
+	return STD forward<FuncArg<func, types...>>(funcArg);
+} 
 
 int main(void)
 {
 	int a = 8;
-	auto sumFunc = addQueue(&sum, 8, a, 7);
+
+	auto func = addQueue(&sum, 4, a, 10);
+
 	a = 10;
-	sumFunc();
+
+	func.call();
 }
